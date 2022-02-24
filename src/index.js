5d6982391e4244a91146aa1f3a6520c7dff40503
Kiiris <barthlemont@gmail.com>
2022-02-24 12:59:39 -0500

    removed gitmodules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { render } from 'react-dom'
+import React from 'react'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
 
 render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 )
